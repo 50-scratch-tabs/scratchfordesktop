259d5a1c1dff1f015fdd931e4f3f36da150a368c
50-scratch-tabs <197407676+50-scratch-tabs@users.noreply.github.com>
2025-03-15 20:20:24 -0500

    You know what? Screw pywebview. We're going tkinter.

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
 import scratchattach
-import webview
+import tkinter as tk
 
-sessions=[]
-
-class JSAPIObject:
+class BetterScratch(tk.Tk):
     def __init__(self):
-        pass
-    def login(self,username,password):
-        print(username,password)
-        try:
-            session=scratchattach.login(username,password)
-        except:
-            return False
-        else:
-            sessions.append(session)
-            return True
-
-js_api=JSAPIObject()
-
-window=webview.create_window("Scratch for desktop",html=open("main.html").read(),js_api=js_api)
-webview.start()
+        super().__init__()
+        self.sessions=[]
