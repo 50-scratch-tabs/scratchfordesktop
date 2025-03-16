a00cf348bf80f77eef23b730b649911361e84174
50-scratch-tabs <197407676+50-scratch-tabs@users.noreply.github.com>
2025-03-15 20:21:09 -0500

    Add main function

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ class BetterScratch(tk.Tk):
     def __init__(self):
         super().__init__()
         self.sessions=[]
+
+def main():
+    app=BetterScratch()
+    app.mainloop()
+
+if __name__=="__main__":
+    main()
