8132746390183239735b09bc63129d4b8afb1fcf
50-scratch-tabs <197407676+50-scratch-tabs@users.noreply.github.com>
2025-03-15 20:15:28 -0500

    Add login features

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import scratchattach
 import webview
 
-window=webview.create_window("Scratch for desktop",html=open("main.html").read())
+sessions=[]
+
+class JSAPIObject:
+    def __init__(self):
+        pass
+    def login(self,username,password):
+        print(username,password)
+        try:
+            session=scratchattach.login(username,password)
+        except:
+            return False
+        else:
+            sessions.append(session)
+            return True
+
+js_api=JSAPIObject()
+
+window=webview.create_window("Scratch for desktop",html=open("main.html").read(),js_api=js_api)
 webview.start()
