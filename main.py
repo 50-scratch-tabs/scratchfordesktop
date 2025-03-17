a692654a29186cbfbc5a96b3ada9aa2bdf1a3908
50-scratch-tabs <197407676+50-scratch-tabs@users.noreply.github.com>
2025-03-17 18:44:58 -0500

    Add open menu

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ class BetterScratchMenuBar(tk.Menu):
         self.create_items()
     def create_items(self):
         self.add_command(label="Thing",command=lambda:print("Thing!!!!"))
+        self.open_menu=tk.Menu(self.master)
+        self.open_menu.add_command(label="Lookup profile...",command=self.master.lookup_user)
+        self.add_cascade(label="Open",menu=self.open_menu)
         self.add_cascade(label="Window",menu=self.master.window_menu)
 class BetterScratchToplevel(tk.Toplevel):
     def __init__(self,master,kind):
