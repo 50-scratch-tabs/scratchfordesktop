bdb8c6f42e1b16c902f006eee60b558326c377ef
50-scratch-tabs <197407676+50-scratch-tabs@users.noreply.github.com>
2025-03-25 19:41:07 -0500

    Fix typo

--- BetterScratch/BetterScratchCommentDisplay.py
+++ BetterScratch/BetterScratchCommentDisplay.py
@@ -7,5 +7,5 @@ class BetterScratchCommentDisplay(tk.Frame):
         i=0
         self.grid_columnconfigure(0,weight=1)
         self.lbl=tk.Label(self,text=commentobj.author().username+" says: "+commentobj.content,wraplength=200, justify="left")
-        self.lbl.grid(row=0,column=0,sticky="nsw")
+        self.lbl.grid(row=0,column=0,sticky="nsew")
         self.lbl.bind('<Configure>', lambda _: self.lbl.config(wraplength=self.winfo_width()))
