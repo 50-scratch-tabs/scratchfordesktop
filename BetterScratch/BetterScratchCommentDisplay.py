0c0e61d480db84da43622bc3791aa6918a13bf47
50-scratch-tabs <197407676+50-scratch-tabs@users.noreply.github.com>
2025-03-25 19:39:07 -0500

    Profile page tweaks, and adding comment display

--- BetterScratch/BetterScratchCommentDisplay.py
+++ BetterScratch/BetterScratchCommentDisplay.py
@@ -4,5 +4,7 @@ class BetterScratchCommentDisplay(tk.Frame):
     def __init__(self,master,commentobj):
         self.master=master
         super().__init__(master)
-        self.lbl=tk.Label(self,text=commentobj.author().username+" says: "+commentobj.content)
-        self.lbl.pack()
+        i=0
+        self.lbl=tk.Label(self,text=commentobj.author().username+" says: "+commentobj.content,wraplength=200, justify="left")
+        self.lbl.pack(fill="both",expand=True)
+        self.lbl.bind('<Configure>', lambda _: self.lbl.config(wraplength=self.winfo_width()))
--- BetterScratch/BetterScratchProfilePage.py
+++ BetterScratch/BetterScratchProfilePage.py
@@ -10,19 +10,21 @@ class BetterScratchProfilePage(BetterScratchToplevel):
         self.sauser=sauser
         self.create_form()
     def create_form(self):
+        self.grid_columnconfigure(0,weight=1,pad=0)
         self.abtmelabel=tk.Label(self,text="About me")
-        self.abtmelabel.grid(row=0,column=0)
+        self.abtmelabel.grid(row=0,column=0,sticky="nsew")
         self.aboutme=tk.Text(self,width=30,height=10)
-        self.aboutme.grid(row=1,column=0)
+        self.aboutme.grid(row=1,column=0,sticky="nsew")
         self.aboutme.insert("0.0",self.sauser.about_me)
         self.aboutme.config(state="disabled")
         self.wiwolabel=tk.Label(self,text="What I'm working on")
-        self.wiwolabel.grid(row=2,column=0)
+        self.wiwolabel.grid(row=2,column=0,sticky="nsew")
         self.wiwo=tk.Text(self,width=30,height=10)
-        self.wiwo.grid(row=3,column=0)
+        self.wiwo.grid(row=3,column=0,sticky="nsew")
         self.wiwo.insert("0.0",self.sauser.wiwo)
         self.wiwo.config(state="disabled")
         self.commentsframe=tk.Frame(self)
-        for i in self.sauser.comments():
-            BetterScratchCommentDisplay(self.commentsframe,i).pack()
-        self.commentsframe.grid(row=4,column=0)
+        self.commentsframe.grid_columnconfigure(0,weight=1)
+        for row,i in enumerate(self.sauser.comments()):
+            BetterScratchCommentDisplay(self.commentsframe,i).grid(row=row,column=0,sticky="nsew")
+        self.commentsframe.grid(row=4,column=0,sticky="nsew")
