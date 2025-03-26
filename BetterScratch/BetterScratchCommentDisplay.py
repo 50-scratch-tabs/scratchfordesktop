6b21e11effb060dd76941902ea89d3b4b8436cfe
50-scratch-tabs <197407676+50-scratch-tabs@users.noreply.github.com>
2025-03-25 19:40:31 -0500

    Make comments smoother

--- BetterScratch/BetterScratchCommentDisplay.py
+++ BetterScratch/BetterScratchCommentDisplay.py
@@ -5,6 +5,7 @@ class BetterScratchCommentDisplay(tk.Frame):
         self.master=master
         super().__init__(master)
         i=0
+        self.grid_columnconfigure(0,weight=1)
         self.lbl=tk.Label(self,text=commentobj.author().username+" says: "+commentobj.content,wraplength=200, justify="left")
-        self.lbl.pack(fill="both",expand=True)
+        self.lbl.grid(row=0,column=0,sticky="nsw")
         self.lbl.bind('<Configure>', lambda _: self.lbl.config(wraplength=self.winfo_width()))
