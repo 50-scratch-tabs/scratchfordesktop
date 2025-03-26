22dea9f7d265444ca63ae07a325f2bed98adcb71
50-scratch-tabs <197407676+50-scratch-tabs@users.noreply.github.com>
2025-03-25 19:17:06 -0500

    I have no clue what this is doing. Let's hope my local changes weren't overwritten.

---
+++ BetterScratch/BetterScratchToplevel.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+from .BetterScratchMenuBar import BetterScratchMenuBar
+
+class BetterScratchToplevel(tk.Toplevel):
+    def __init__(self,master,kind):
+        self.master.windowmenuitems.append(self)
+        super().__init__(master)
+        self.master=master
+        self.menu=BetterScratchMenuBar(self.master,kind)
+        self.config(menu=self.menu)
+        self.master.window_menu.add_radiobutton(label=self.title(),command=self.focus)
+        self.protocol("WM_DELETE_WINDOW",self.close)
+        self.minsize(180,100)
+        self.geometry("180x100")
+    def close(self):
+        self.master.window_menu.delete(self.master.windowmenuitems.index(self)+1)
+        del self.master.windowmenuitems[self.master.windowmenuitems.index(self)]
+        self.destroy()
+    def title(self,title=None):
+        if title is not None:
+            self.master.window_menu.entryconfigure(self.master.windowmenuitems.index(self)+1,label=title)
+        return super().title(title)
